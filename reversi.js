7c0dabb6c223dbab40771a8918b3716113e1a3d6
Jan Mrazek <mrazeja@fel.cvut.cz>
2017-09-13 22:26:10 +0200

    checkVictory, next turn, pass turn, playable now

--- reversi.js
+++ reversi.js
@@ -7,26 +7,38 @@ var state = {
   enemy: 'w',
   board: [
     [null, null, null, null, null, null, null, null],
-    [null, null, null, 'w', null, null, null, null],
-    [null, 'w', null, null, 'w', 'w', null, null],
-    [null, null, 'w', 'w', 'w', 'b', null, null],
-    [null, null, 'w', 'b', 'w', 'w', 'w', null],
-    [null, null, 'w', 'w', 'w', null, null, null],
-    [null, 'w', null, null, 'b', 'w', null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, 'w', 'b', null, null, null],
+    [null, null, null, 'b', 'w', null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null]
   ],
   captures: {w: 0, b: 0}
 }
 
-// TODO
-function checkForVictory() {
-  if(state.captures.w == 20) {
-    state.over = true;
-    return 'black wins';
+function checkForVictory() 
+{
+  var whiteScore = 0;
+  var blackScore = 0;
+  for (var i = 0; i < state.board.length; i++) 
+  {
+    for (var j = 0; j < state.board[i].length; j++)
+    {
+      if (state.board[i][j] == 'w')
+        whiteScore++;
+      else if (state.board[i][j] == 'b')
+        blackScore++;
+    }
   }
-  if(state.captures.b == 20) {
-    state.over = true;
-    return 'white wins';
+  if (whiteScore+blackScore == state.board.length*state.board.length)
+  {
+    if (whiteScore > blackScore)
+      return 'white wins';
+    else if (whiteScore < blackScore)
+      return 'black wins';
+    else return 'tie';
   }
   return null;
 }
@@ -39,36 +51,13 @@ function nextTurn() {
 
 function deselectAll()
 {
-  var allCheckers = document.querySelectorAll('.possible');//document.getElementsByClassName('selected');
-  //console.log("len: " + allCheckers.length);
-  //console.log(allCheckers);
+  var allCheckers = document.querySelectorAll('.possible');
 
   for (var i = allCheckers.length-1; i >= 0 ; i--)
   {
     allCheckers[i].classList.remove('possible');
   }
 }
-
-function checkHorizontalR(x, y)
-{
-  var streak = 0;
-  var possible = false;
-  var enemyPieces = [];
-  for (var k = y+1; k < state.board.length; k++) 
-  {
-    if (state.board[x][k] == state.enemy)
-    {
-      streak++;
-      enemyPieces.push({x: x, y: k});
-    }
-    if ((streak == (k-y-1)) && streak != 0 && state.board[x][k]==state.turn)
-    {
-      return enemyPieces;
-    }
-  }
-  return [];
-}
-
 
 function checkHorizontalL(x, y)
 {
@@ -90,20 +79,19 @@ function checkHorizontalL(x, y)
   return [];
 }
 
-
-function checkVerticalD(x, y)
+function checkHorizontalR(x, y)
 {
   var streak = 0;
   var possible = false;
   var enemyPieces = [];
-  for (var k = x+1; k < state.board.length; k++) 
+  for (var k = y+1; k < state.board.length; k++) 
   {
-    if (state.board[k][y] == state.enemy)
+    if (state.board[x][k] == state.enemy)
     {
       streak++;
-      enemyPieces.push({x: k, y: y});
+      enemyPieces.push({x: x, y: k});
     }
-    if ((streak == (k-x-1)) && streak != 0 && state.board[k][y]==state.turn)
+    if ((streak == (k-y-1)) && streak != 0 && state.board[x][k]==state.turn)
     {
       return enemyPieces;
     }
@@ -131,8 +119,27 @@ function checkVerticalU(x, y)
   return [];
 }
 
+function checkVerticalD(x, y)
+{
+  var streak = 0;
+  var possible = false;
+  var enemyPieces = [];
+  for (var k = x+1; k < state.board.length; k++) 
+  {
+    if (state.board[k][y] == state.enemy)
+    {
+      streak++;
+      enemyPieces.push({x: k, y: y});
+    }
+    if ((streak == (k-x-1)) && streak != 0 && state.board[k][y]==state.turn)
+    {
+      return enemyPieces;
+    }
+  }
+  return [];
+}
 
-function checkDiagonalSE(x, y) //ok
+function checkDiagonalSE(x, y)
 {
   var streak = 0;
   var possible = false;
@@ -232,14 +239,12 @@ function highlightMove(x, y)
 
 function highlightOnHover(event)
 {
-  deselectAll();
   var x = parseInt(event.target.id.charAt(7));
   var y = parseInt(event.target.id.charAt(9));
   if (isNaN(x))
     return;
   if (state.board[x][y])
     return;
-  //console.log("Hovering over " + x + " " + y);
   var pieces = getEnemyPieces(x, y);
 
   pieces.forEach(function(piece) {
@@ -248,7 +253,6 @@ function highlightOnHover(event)
 
   if (pieces.length > 0)
     highlightMove(x, y);
-
 }
 
 function getEnemyPieces(x, y)
@@ -291,6 +295,12 @@ function applyMove(event, x, y)
   checker.classList.add('checker-' + state.board[x][y]);
   square.appendChild(checker);
 
+  var vic = checkForVictory();
+  if (vic)
+  {
+    alert(vic);
+    location.reload(); //play again
+  }
   nextTurn();
 }
 
@@ -305,7 +315,12 @@ function handleSquareClick(event)
   applyMove(event, x, y);
 }
 
-// ok
+function handlePassClick(event)
+{
+  event.preventDefault();
+  nextTurn();
+}
+
 function setup()
 {
   var board = document.createElement('section');
@@ -320,6 +335,7 @@ function setup()
       square.classList.add('reversi-square');
       square.onclick = handleSquareClick;      
       square.onmouseover = highlightOnHover;
+      square.onmouseleave = deselectAll;
       if (state.board[i][j])
       {
         var checker = document.createElement('div');
@@ -330,6 +346,12 @@ function setup()
       board.appendChild(square);
     }
   }
+  var pass = document.createElement('div');
+  pass.id = ('pass-square');
+  pass.classList.add('pass-square');
+  pass.appendChild(document.createTextNode("Pass turn"));
+  pass.onclick = handlePassClick;
+  board.appendChild(pass);
 }
 
 setup();
